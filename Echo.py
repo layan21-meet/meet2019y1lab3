1382d9c1ef2e6e8efdbf5737ed7d9560dd0a9513
layan21-meet <layan21@meet.mit.edu>
2019-07-23 03:25:58 -0400

    Stopping here

---
+++ Echo.py
@@ -0,0 +1,15 @@
+
+
+person_one = input("What is the age of the girl?")
+person_two = input("What is the age of the boy?")
+person_one = int(person_one)
+person_two = int(person_two)
+age_difference = person_two - person_one
+age_difference = (abs(age_difference))
+print(age_difference)
+
+
+
+
+
+
---
+++ agecalculations.py
@@ -0,0 +1,17 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: /home/student/layan21_lab3/meet2019y1lab3/Echo.py =========
+What is the age of the girl? 15 
+What is the age of the boy? 20
+>>> 
+========= RESTART: /home/student/layan21_lab3/meet2019y1lab3/Echo.py =========
+What is the age of the girl?20
+What is the age of the boy?15
+>>> 
+========= RESTART: /home/student/layan21_lab3/meet2019y1lab3/Echo.py =========
+What is the age of the girl? 15
+What is the age of the boy? 20
+5
+>>> 
